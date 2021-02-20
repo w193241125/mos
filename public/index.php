319a1d16da044aaceca6f9b304ad81a04d446866
larwas <164722305@qq.com>
2021-02-20 11:04:49 +0800

    1

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ define('LARAVEL_START', microtime(true));
 | loading any of our classes later on. It feels great to relax.
 |
 */
-
+require __DIR__.'/../app/helper/helper.php';//辅助函数
 require __DIR__.'/../vendor/autoload.php';
 
 /*
